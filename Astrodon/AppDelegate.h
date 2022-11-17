8f95e3714ad01275b2d0ba0215d6bf02234a6010
Dominik Hauser <dom@dasdom.de>
2022-11-17 18:07:15 +0100

    Initial Commit

---
+++ Astrodon/AppDelegate.h
@@ -0,0 +1,11 @@
+//  Created by Dominik Hauser on 17.11.22.
+//  
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Astrodon/main.m
@@ -0,0 +1,12 @@
+//  Created by Dominik Hauser on 17.11.22.
+//  
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+  }
+  return NSApplicationMain(argc, argv);
+}
