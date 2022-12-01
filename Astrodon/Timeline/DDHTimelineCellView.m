351ef8c968288b26ab9490f50ca6c8ec64839c53
Dominik Hauser <dom@dasdom.de>
2022-12-01 11:11:08 +0100

    Revert "Init timeline cell in code"
    This reverts commit bcffbcc0009b91c22efb5fbfa0a282977ebcb0df.

--- Astrodon/Timeline/DDHTimelineCellView.m
+++ Astrodon/Timeline/DDHTimelineCellView.m
@@ -9,38 +9,6 @@
 
 @implementation DDHTimelineCellView
 
-- (instancetype)initWithFrame:(NSRect)frameRect {
-  if (self = [super initWithFrame:frameRect]) {
-    _displayNameTextField = [[NSTextField alloc] init];
-    _acctTextField = [[NSTextField alloc] init];
-
-    NSStackView *nameStackView = [NSStackView stackViewWithViews:@[_displayNameTextField, _acctTextField]];
-    nameStackView.orientation = NSUserInterfaceLayoutOrientationVertical;
-
-    _showMoreButton = [[NSButton alloc] init];
-    [_showMoreButton setButtonType:NSButtonTypeMomentaryPushIn];
-    [_showMoreButton setImagePosition:NSNoImage];
-    [_showMoreButton setBezelStyle:NSBezelStyleRounded];
-    _showMoreButton.title = @"show more";
-
-    NSStackView *textStackView = [NSStackView stackViewWithViews:@[nameStackView, self.textField, _showMoreButton]];
-    textStackView.orientation = NSUserInterfaceLayoutOrientationVertical;
-
-    NSStackView *stackView = [NSStackView stackViewWithViews:@[self.imageView, textStackView]];
-    stackView.translatesAutoresizingMaskIntoConstraints = NO;
-
-    [self addSubview:stackView];
-
-    [NSLayoutConstraint activateConstraints:@[
-      [stackView.topAnchor constraintEqualToAnchor:self.topAnchor constant:8],
-      [stackView.leadingAnchor constraintEqualToAnchor:self.leadingAnchor constant:8],
-      [stackView.bottomAnchor constraintEqualToAnchor:self.bottomAnchor constant:-8],
-      [stackView.trailingAnchor constraintEqualToAnchor:self.trailingAnchor constant:-8],
-    ]];
-  }
-  return self;
-}
-
 - (void)drawRect:(NSRect)dirtyRect {
     [super drawRect:dirtyRect];
     
